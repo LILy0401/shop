25c42acdb5d0f517f6501e0b72e00d43d64b8a78
weiwei <3092836428@163.com>
2018-12-26 16:08:30 +0800

    框架

---
+++ src/js/a.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: mikey.zhaopeng 
+ * @Date: 2018-10-12 10:51:33 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2018-10-26 11:40:42
+ */
+/**
+ * [这是一个求和函数]
+ * @param {[Number]}  a   [任意自然数]
+ * @param {[Number]}   b    [RENSH]
+ * @return {[type]}
+ */
+function sum(a, b) {
+    return a + b;
+}
